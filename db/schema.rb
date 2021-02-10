672813fd68e9db000d2374745c148deedaecb19e
RaySatuma <sttrppasasa@gmail.com>
2021-02-10 19:55:42 +0900

    Add posts table to title,youtube_url,note

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_073520) do
+ActiveRecord::Schema.define(version: 2021_02_10_105321) do
 
   create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2021_02_01_073520) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "youtube_url"
+    t.text "note"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
