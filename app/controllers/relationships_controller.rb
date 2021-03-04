3102eee1925593186cd6470fad7a11d4c514b6c5
RaySatuma <sttrppasasa@gmail.com>
2021-03-04 21:19:23 +0900

    add files

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,21 @@
 class RelationshipsController < ApplicationController
-  before_action :require_user_logged_in
+  before_action :require_user_logged_in, :set_id
   
   def create
     user = User.find(params[:follow_id])
     current_user.follow(user)
-    flash[:success] = 'ユーザーをフォローしました'
-    redirect_back(fallback_location: root_path)
+    flash.now[:success] = 'ユーザーをフォローしました'
   end
 
   def destroy
     user = User.find(params[:follow_id])
     current_user.unfollow(user)
-    flash[:success] = 'ユーザーのフォローを解除しました'
-    redirect_back(fallback_location: root_path)
+    flash.now[:success] = 'ユーザーのフォローを解除しました'
+  end
+
+private
+
+  def set_id
+    @user = User.find(params[:follow_id])
   end
 end
