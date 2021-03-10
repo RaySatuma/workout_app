3acec8be7d47d4d3267d64f009d6358130dd540e
RaySatuma <sttrppasasa@gmail.com>
2021-03-10 11:13:01 +0900

    Change comment-function to async

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,38 @@
 class CommentsController < ApplicationController
   before_action :require_user_logged_in
 
+ 
   def create
-    post = Post.find(params[:post_id])
-    @comment = post.comments.build(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
+    @comments = @post.comments
+    @userf = @post.user_id
+    @user = User.find_by(id: @userf)
+    
     if @comment.save
-      flash[:success] = 'コメントしました。'
-      redirect_back(fallback_location: root_path)
+      @comment = Comment.new
+      flash.now[:success] = 'コメントしました。'
     else
-      flash[:success] = 'コメントできませんでした。'
-      redirect_back(fallback_location: root_path)
+      flash.now[:success] = 'コメントできませんでした。'
     end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @userf = @post.user_id
+    @user = User.find_by(id: @userf)
+    @comments = @post.comments
     @comment = Comment.find(params[:id])
     if @comment.destroy
-      flash[:success] = 'コメントを削除しました。'
-      redirect_back(fallback_location: root_path)
+      @comment = Comment.new
+      flash.now[:success] = 'コメントを削除しました。'
     else
       flash.now[:danger] = "コメント削除に失敗しました。"
       render :post
     end
   end
+
 
 private
 
